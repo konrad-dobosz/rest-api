26586d2f090d00c22c42e5a0684a59cb0d471828
konrad-dobosz <crisps.taffies.0w@icloud.com>
2022-12-18 15:29:16 +0100

    Add routes to api.php
    Add new routes from PeopleController

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\PeopleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('people', [PeopleController::class, 'index']);
+Route::get('people/{people}', [PeopleController::class, 'show']);
+Route::post('people', [PeopleController::class, 'store']);
+Route::put('people/{people}', [PeopleController::class, 'update']);
+Route::delete('people/{people}', [PeopleController::class, 'delete']);
